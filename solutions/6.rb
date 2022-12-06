6a29240dfc055da7e8be2aeaf692b420e09b5ba9
Dovy <djasulaitis05@gmail.com>
2022-12-06 12:39:47 +0100

    add day 6

--- solutions/6.rb
+++ solutions/6.rb
@@ -1,5 +1,5 @@
 input = File.read("input6")
 # 1st star
-puts input.chars.inject(0){|s, x|input[s..s+3].chars != input[s..s+3].chars.uniq ? s + 1 : s} + 4
+puts input.chars.inject(0){|s, x|input[s..s + 3].chars != input[s..s + 3].chars.uniq ? s + 1 : s} + 4
 # 2nd star
-puts input.chars.inject(0){|s, x|input[s..s+13].chars != input[s..s+13].chars.uniq ? s + 1 : s} + 14
+puts input.chars.inject(0){|s, x|input[s..s + 13].chars != input[s..s + 13].chars.uniq ? s + 1 : s} + 14
